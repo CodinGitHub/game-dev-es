75a95223c101b151482c13e701f17b27f9c24044
David <gms2programador@gmail.com>
2023-03-21 21:22:49 -0500

    tailwind configurado

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,15 @@ import './App.css'
 
 
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
       <MenuTitle title="Uno" />
-      <MenuTitle title="Dos" />
-      <h1>JSGameDev</h1>
+
+      <h1><span className="text-5xl font-bold">JS</span>GameDev<span className="text-5xl font-bold">ES</span></h1>
       <h2>Portal en español sobre desarrollo de video juegos con Javascript</h2>
       <p>Este sitio web está desarrollado usando React, Typescript, Vite</p>
     </div>
